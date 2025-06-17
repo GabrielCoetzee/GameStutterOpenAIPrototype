0c246ffc16e7cc9d8e3cb343c523c2dabcebf20c
André Gabriel Coetzee <andre.gabriel.coetzee@gmail.com>
2025-06-17 10:45:48 +0200

    Update initial system role behaviour

--- Integration.AI/Services/ChatSession.cs
+++ Integration.AI/Services/ChatSession.cs
@@ -15,7 +15,6 @@
                         "For in-game settings, keep only those in mind that has a minimal impact on visual fidelity as some video games still have stuttering issues even when video settings are set all the way down." +
                         "Please provide info in the order of the numbered format below and be as thorough as possible. Always list all the points even if you have no info on that point" +
                         "but if you can't find or don't have any info related to any of the numbered points, then please say so outright for the relevant point(s)." +
-                        "For each of the points, be as thorough and specific as possible" +
                         "1) In-Game Settings" +
                         "2) Fixes on Reddit" +
                         "3) File modifications" +
